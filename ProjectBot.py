df21f7bf255f9c45dff433e4a006a5d3efe31896
Sergey <108259528+Maclovi@users.noreply.github.com>
2022-12-27 18:40:04 +0300

    Update ProjectBot.py

--- ProjectBot.py
+++ ProjectBot.py
@@ -1,11 +1,12 @@
 import logging
 from asyncio import sleep
-from aiogram import Bot, Dispatcher, executor, types
+from aiogram import Bot, Dispatcher, executor, types, utils
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.dispatcher.filters import Text
 from os import remove as remove_file, environ
-from myModule import GetMusicYoutube
+from pytube import YouTube, exceptions
 from keep_alive import keep_alive
+from random import choice
 
 
 #Button keyboard
@@ -13,7 +14,8 @@ kb = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False)
 kb1 = KeyboardButton('Помощь🚒')
 kb2 = KeyboardButton('Легально?⚠')
 kb3 = KeyboardButton('Свой бот🤖')
-kb.add(kb1, kb2, kb3)
+kb4 = KeyboardButton('Шар предсказания🎱')
+kb.add(kb4).add(kb1, kb2, kb3)
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -23,6 +25,18 @@ channel_user = environ['Channel_user']
 channel_music = environ['Channel_music']
 bot = Bot(token=environ['key'])
 dp = Dispatcher(bot)
+
+
+# My func
+def GetMusicYoutube(link):
+  '''Получение audio из youtube'''
+  yt = YouTube(link)
+  stream = yt.streams.get_audio_only()           #get_itag(140)
+  if stream.filesize_mb > 50:
+    raise utils.exceptions.NetworkError('Размер файлы превышает 50мб')
+  obj = stream.download(filename=yt.title)
+  return obj, yt.views, str(yt.publish_date).split('-')[0], \
+  yt.thumbnail_url
 
 
 async def on_startup(_):
@@ -50,7 +64,7 @@ async def start_command(message: types.Message) -> None:
   answ = await message.answer(desc, parse_mode='HTML', reply_markup=kb)
   await message.delete()
   if message.text == r'/start':
-      await bot.send_message(channel_user,f'''
+    await bot.send_message(channel_user,f'''
 {message.from_user.id} - ID пользователя
 {message.from_user.first_name} - Имя пользователя
 {message.from_user.last_name} - Фамилия или псевдоним пользователя
@@ -61,7 +75,7 @@ async def start_command(message: types.Message) -> None:
 
 
 @dp.message_handler(Text(equals='Помощь🚒'))
-async def HelpUser(message: types.Message) -> None:
+async def helper(message: types.Message) -> None:
   await start_command(message)
 
 
@@ -69,8 +83,9 @@ async def HelpUser(message: types.Message) -> None:
 async def Legal(message: types.Message) -> None:
   answ = f'''Я создал BotMusic с идеей, что должен существовать легальный
 инструмент для записи потоковой передачи в Интернете, который был бы чистым,
-простым и не содержал спама. Согласно EFF.org, «в законе ясно, что простое предоставление общественности инструмента для копирования цифровых носителей
-не влечет за собой ответственности за авторские права».
+простым и не содержал спама. Согласно EFF.org, «в законе ясно,
+что простоепредоставление общественности инструмента для копирования
+цифровых носителей не влечет за собой ответственности за авторские права».
 
 {'-' * 20}
 <em>(Сообщение удалится автоматически)</em>'''
@@ -96,6 +111,21 @@ async def OwnBot(message: types.Message) -> None:
   await bot.delete_message(message.chat.id, answ.message_id)
 
 
+@dp.message_handler(Text(equals='Шар предсказания🎱'))
+async def HelpUser(message: types.Message) -> None:
+  answers = ['Бесспорно', 'Мне кажется - да', 'Пока неясно, попробуй снова',
+             'Даже не думай', 'Предрешено', 'Вероятнее всего', 'Спроси позже',
+             'Мой ответ - нет', 'Никаких сомнений', 'Хорошие перспективы',
+             'Лучше не рассказывать', 'По моим данным - нет', 'Определённо да',
+             'Знаки говорят - да', 'Сейчас нельзя предсказать',
+             'Перспективы не очень хорошие', 'Можешь быть уверен в этом', 'Да',
+             'Сконцентрируйся и спроси опять', 'Весьма сомнительно']
+  answ = await message.answer(choice(answers))
+  await sleep(5)
+  await bot.delete_message(message.chat.id, answ.message_id)
+  await message.delete()
+
+
 @dp.message_handler(Text(startswith=('https://youtu.be/',
                                      'https://www.youtube.com/'),
                          ignore_case=True))
@@ -106,24 +136,28 @@ async def send_song(message: types.Message) -> None:
   await message.delete()
   bot_answ = await message.answer('<em><b>Ждите...</b></em>',
                                   parse_mode='HTML')
-  
   try:
     obj, views, pub, URL = [i for i in GetMusicYoutube(message.text)]
-    description = f'<em>Столько: {views:,d} людей прослушали</em>.\n' \
-                  f'<em>Дата публикации: {pub}г</em>.'
-
+    description = \
+        f'<em>Столько: {views:,d} людей прослушали</em>.\n' \
+        f'<em>Дата публикации: {pub}г</em>.'
     with open(obj, 'rb') as file1:
       # С помощью переменной, можно узнать id сообщения и не только
       msg = await message.answer_audio(
         file1,
         caption=description,
         parse_mode='HTML',
-        thumb=types.InputFile.from_url(URL)
-      )
-  except:
+        thumb=types.InputFile.from_url(URL))
+    remove_file(obj)
+    await bot.forward_message(
+      chat_id=channel_music,            # Указать конечный канал
+      from_chat_id=message.chat.id,  # Указать начальный канал
+      message_id=msg.message_id)      # Указать id сообщения
+    
+  except exceptions.VideoUnavailable:   
     answ = f'''
 Отклонено правами автора.
-Пожалуйста, попробуйте отправить другую ссылку:
+Пожалуйста, отправить другую ссылку:
 
 {'-' * 20}
 <em>(Сообщение удалится автоматически)</em>'''
@@ -131,14 +165,20 @@ async def send_song(message: types.Message) -> None:
     # Автоудаление через 30сек сообщения бота <answ>
     await sleep(30)
     await bot.delete_message(message.chat.id, answ.message_id)
-  else:
-    remove_file(obj)
-    await bot.forward_message(
-      chat_id=channel_music,            # Указать конечный канал
-      from_chat_id=message.chat.id,  # Указать начальный канал
-      message_id=msg.message_id      # Указать id сообщения
-    )
-  await bot.delete_message(message.chat.id, bot_answ.message_id)
+    
+  except utils.exceptions.NetworkError:
+    answ = f'''
+Отклонено, размер файла превышает 50мб.
+Пожалуйста, отправить другую ссылку:
+
+{'-' * 20}
+<em>(Сообщение удалится автоматически)</em>'''
+    answ = await message.answer(answ, parse_mode='HTML')
+    # Автоудаление через 30сек сообщения бота <answ>
+    await sleep(30)
+    await bot.delete_message(message.chat.id, answ.message_id)
+  finally:
+    await bot.delete_message(message.chat.id, bot_answ.message_id)
 
 
 if __name__ == '__main__':
